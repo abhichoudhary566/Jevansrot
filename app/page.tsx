d0d26c9d10c0ee0b25c80d49d8774a1cd6c63871
Sunil Rawat <info@okaytrip.in>
2026-02-09 19:05:39 +0530

    error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import TopBar from "@/components/layout/TopBar"
 import Navbar from "@/components/layout/Navbar"
 import Hero from "@/components/home/Hero"
-import FeaturedProducts from "@/components/home/FeaturedProducts"
+
 import Footer from "@/components/layout/Footer"
 import ProductsSection from "@/components/home/ProductsSection"
 import HealthSolutionsSection from "@/components/home/HealthSolutionsSection"
